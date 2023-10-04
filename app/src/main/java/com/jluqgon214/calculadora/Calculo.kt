c1e5bf33643f02c22c751dc8bf1936570157d3b7
José Manuel Luque González <jluqgon214@g.educaand.es>
2023-10-05 01:18:34 +0200

    Ya Funciona

--- app/src/main/java/com/jluqgon214/calculadora/Calculo.kt
+++ app/src/main/java/com/jluqgon214/calculadora/Calculo.kt
@@ -1,6 +1,63 @@
 package com.jluqgon214.calculadora
 
-class Calculo(num1: Int, num2: Int, resultado: Int, operacion: String) {
+class Calculo(num1: Int, num2: Int, res: Int, op: String) {
 
+    var numero1 = num1
 
+    var numero2 = num2
+
+    var resultado = res
+
+    var operacion = op
+
+    init {
+        numero1 = num1
+        numero2 = num2
+        resultado = res
+        operacion = op
+    }
+
+    fun Calcular(operacion: String): Int{
+        var num1 = ""
+        var num2 = ""
+        var antes = false
+
+        for(numero in operacion) {
+            if (numero.isDigit()) {
+                if (antes == false) {
+                    num1 = num1 + numero
+                }
+                else {
+                    num2 = num2 + numero
+                }
+            }
+
+            if (numero.toString() == "+" || numero.toString() == "-" || numero.toString() == "/" || numero.toString() == "x") {
+                antes = true
+            }
+        }
+
+        var resultado = 0
+
+        for (signo in operacion) {
+            if (signo.toString() == "+"){
+                resultado = num1.toInt() + num2.toInt()
+                break
+            }
+            if (signo.toString() == "-"){
+                resultado = num1.toInt() - num2.toInt()
+                break
+            }
+            if (signo.toString() == "x"){
+                resultado = num1.toInt() * num2.toInt()
+                break
+            }
+            if (signo.toString() == "/"){
+                resultado = num1.toInt() / num2.toInt()
+                break
+            }
+        }
+
+        return resultado
+    }
 }
--- app/src/main/java/com/jluqgon214/calculadora/MainActivity.kt
+++ app/src/main/java/com/jluqgon214/calculadora/MainActivity.kt
@@ -11,7 +11,23 @@ class MainActivity : AppCompatActivity() {
     //Declaración de variables
     private lateinit var resultado: TextView
     private lateinit var boton_CE: Button
-    var resultadoString = ""
+    private lateinit var boton_igual: Button
+    private lateinit var boton_mas: Button
+    private lateinit var boton_menos: Button
+    private lateinit var boton_multiplicar: Button
+    private lateinit var boton_dividir: Button
+    private lateinit var boton1: Button
+    private lateinit var boton2: Button
+    private lateinit var boton3: Button
+    private lateinit var boton4: Button
+    private lateinit var boton5: Button
+    private lateinit var boton6: Button
+    private lateinit var boton7: Button
+    private lateinit var boton8: Button
+    private lateinit var boton9: Button
+    private lateinit var boton0: Button
+
+    var calculo = Calculo(0, 0, 0, "")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,15 +37,135 @@ class MainActivity : AppCompatActivity() {
         //Inicializamos las variables al inicio de la App
         resultado = findViewById(R.id.resultado)
         boton_CE = findViewById(R.id.boton_ce)
+        boton_igual = findViewById(R.id.boton_igual)
+        boton_mas = findViewById(R.id.boton_mas)
+        boton_menos = findViewById(R.id.boton_menos)
+        boton_multiplicar = findViewById(R.id.boton_multiplicar)
+        boton_dividir = findViewById(R.id.boton_dividir)
+        boton1 = findViewById(R.id.boton_1)
+        boton2 = findViewById(R.id.boton_2)
+        boton3 = findViewById(R.id.boton_3)
+        boton4 = findViewById(R.id.boton_4)
+        boton5 = findViewById(R.id.boton_5)
+        boton6 = findViewById(R.id.boton_6)
+        boton7 = findViewById(R.id.boton_7)
+        boton8 = findViewById(R.id.boton_8)
+        boton9 = findViewById(R.id.boton_9)
+        boton0 = findViewById(R.id.boton_0)
 
         boton_CE.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                boton_CE()
+                calculo.operacion = ""
+                resultado.text = "0"
             }
         })
-    }
 
-    fun boton_CE (){
-        resultado.text = "0"
+        boton_igual.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                println(calculo.Calcular(calculo.operacion))
+
+                resultado.text = calculo.Calcular(calculo.operacion).toString()
+
+                calculo.operacion = ""
+            }
+        })
+
+        boton1.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                calculo.operacion += 1
+                resultado.text = calculo.operacion
+            }
+        })
+
+        boton2.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                calculo.operacion += 2
+                resultado.text = calculo.operacion
+            }
+        })
+
+        boton3.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                calculo.operacion += 3
+                resultado.text = calculo.operacion
+            }
+        })
+
+        boton4.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                calculo.operacion += 4
+                resultado.text = calculo.operacion
+            }
+        })
+
+        boton5.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                calculo.operacion += 5
+                resultado.text = calculo.operacion
+            }
+        })
+
+        boton6.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                calculo.operacion += 6
+                resultado.text = calculo.operacion
+            }
+        })
+
+        boton7.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                calculo.operacion += 7
+                resultado.text = calculo.operacion
+            }
+        })
+
+        boton8.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                calculo.operacion += 8
+                resultado.text = calculo.operacion
+            }
+        })
+
+        boton9.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                calculo.operacion += 9
+                resultado.text = calculo.operacion
+            }
+        })
+
+        boton0.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                calculo.operacion += 0
+                resultado.text = calculo.operacion
+            }
+        })
+
+        boton_mas.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                calculo.operacion += "+"
+                resultado.text = calculo.operacion
+            }
+        })
+
+        boton_menos.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                calculo.operacion += "-"
+                resultado.text = calculo.operacion
+            }
+        })
+
+        boton_multiplicar.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                calculo.operacion += "x"
+                resultado.text = calculo.operacion
+            }
+        })
+
+        boton_dividir.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                calculo.operacion += "/"
+                resultado.text = calculo.operacion
+            }
+        })
     }
 }
